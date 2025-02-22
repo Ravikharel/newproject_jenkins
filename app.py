b5fb863c2d8c16ae31bd7121385ec12779eb4452
Ravikharel <ravikharel77@gmail.com>
2025-02-22 08:40:39 +0000

    Pushed

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-from flask import Flask
+print("Hello world") 
+print("This is the message written to jenkins") 
 
-# Create a Flask app
-app = Flask(__name__)
-
-# Define a route for the homepage
-@app.route('/')
-def home():
-    return "Hello, World! Welcome to my Flask website."
-
-# Run the app
-if __name__ == '__main__':
-    # Use your machine's IP address and set port (default is 5000)
-    app.run(host='0.0.0.0', port=5000, debug=True)
